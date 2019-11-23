16c44b103d03d455acc9371e0c4176919be2131e
kinnerapriyap <kinnerapriyap@gmail.com>
2019-11-24 08:38:32 +0900

    Initial commit

---
+++ projects-by-day/day-2-guess-the-flag/day-2-guess-the-flag/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  day-2-guess-the-flag
+//
+//  Created by kinnerapriyap on 2019/11/24.
+//  Copyright © 2019 kinnerapriyap. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
