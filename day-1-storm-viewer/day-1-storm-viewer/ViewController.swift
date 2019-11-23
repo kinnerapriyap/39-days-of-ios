19757475a2be10b67a1e5fad4dc430986350b725
kinnerapriyap <kinnerapriyap@gmail.com>
2019-11-23 17:47:34 +0900

    Initial commit

---
+++ day-1-storm-viewer/day-1-storm-viewer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  day-1-storm-viewer
+//
+//  Created by kinnerapriyap on 2019/11/23.
+//  Copyright © 2019 kinnerapriyap. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
