3edc78fe2d498c0614a3e4c46e133a8aa3b6b278
kinnerapriyap <kinnerapriyap@gmail.com>
2019-11-23 18:38:35 +0900

    Create DetailViewController

---
+++ day-1-storm-viewer/day-1-storm-viewer/DetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailViewController.swift
+//  day-1-storm-viewer
+//
+//  Created by kinnerapriyap on 2019/11/23.
+//  Copyright © 2019 kinnerapriyap. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
