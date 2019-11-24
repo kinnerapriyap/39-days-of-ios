89db61570068fb0cc0292df52e65d0383513bbdd
kinnerapriyap <kinnerapriyap@gmail.com>
2019-11-24 09:10:56 +0900

    Add countries to controller

--- projects-by-day/day-2-guess-the-flag/day-2-guess-the-flag/ViewController.swift
+++ projects-by-day/day-2-guess-the-flag/day-2-guess-the-flag/ViewController.swift
@@ -16,9 +16,13 @@ class ViewController: UIViewController {
     
     @IBOutlet var button3: UIButton!
     
+    var countries = [String]()
+    var score = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        
     }
 
 
