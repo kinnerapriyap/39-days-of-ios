8455926bd08bbab96b72a529706e0769e61cb963
kinnerapriyap <kinnerapriyap@gmail.com>
2019-11-24 10:27:39 +0900

    Show dialog with score

--- projects-by-day/day-2-guess-the-flag/day-2-guess-the-flag/ViewController.swift
+++ projects-by-day/day-2-guess-the-flag/day-2-guess-the-flag/ViewController.swift
@@ -35,7 +35,7 @@ class ViewController: UIViewController {
         askQuestion()
     }
 
-    func askQuestion() {
+    func askQuestion(action: UIAlertAction! = nil) {
         countries.shuffle()
         button1.setImage(UIImage(named: countries[0]), for: .normal)
         button2.setImage(UIImage(named: countries[1]), for: .normal)
@@ -45,7 +45,19 @@ class ViewController: UIViewController {
     }
     
     @IBAction func buttonTapped(_ sender: UIButton) {
+        var title: String
+
+        if sender.tag == correctAnswer {
+            title = "Correct"
+            score += 1
+        } else {
+            title = "Wrong"
+            score -= 1
+        }
         
+        let ac = UIAlertController(title: title, message: "Your score is \(score).", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        present(ac, animated: true)
     }
 }
 
