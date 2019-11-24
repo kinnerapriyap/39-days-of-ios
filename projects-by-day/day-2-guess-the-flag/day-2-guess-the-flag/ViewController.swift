3d841ae347cee26a686b201231598f80a969053e
kinnerapriyap <kinnerapriyap@gmail.com>
2019-11-24 09:16:44 +0900

    Set image and border to flags

--- projects-by-day/day-2-guess-the-flag/day-2-guess-the-flag/ViewController.swift
+++ projects-by-day/day-2-guess-the-flag/day-2-guess-the-flag/ViewController.swift
@@ -23,8 +23,21 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
         
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        button3.layer.borderColor = UIColor.lightGray.cgColor
+        
+        askQuestion()
     }
 
-
+    func askQuestion() {
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+    }
 }
 
