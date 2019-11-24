6d14ad862e9148e73d92cb2cb748c1942a1799e7
kinnerapriyap <kinnerapriyap@gmail.com>
2019-11-24 10:31:46 +0900

    Challenge1 add score to nav bar

--- projects-by-day/day-2-guess-the-flag/day-2-guess-the-flag/ViewController.swift
+++ projects-by-day/day-2-guess-the-flag/day-2-guess-the-flag/ViewController.swift
@@ -41,7 +41,7 @@ class ViewController: UIViewController {
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for: .normal)
         correctAnswer = Int.random(in: 0...2)
-        title = countries[correctAnswer].uppercased()
+        title = countries[correctAnswer].uppercased() + "  Score: \(score)"
     }
     
     @IBAction func buttonTapped(_ sender: UIButton) {
