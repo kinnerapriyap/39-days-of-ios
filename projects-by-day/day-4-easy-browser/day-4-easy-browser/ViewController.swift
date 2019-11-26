f13394d43e8100c98e2ebee0df06c5012ea6d910
kinnerapriyap <kinnerapriyap@gmail.com>
2019-11-26 15:27:52 +0900

    Day 4 initial commit

---
+++ projects-by-day/day-4-easy-browser/day-4-easy-browser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  day-4-easy-browser
+//
+//  Created by kinnerapriyap on 2019/11/26.
+//  Copyright © 2019 kinnerapriyap. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
