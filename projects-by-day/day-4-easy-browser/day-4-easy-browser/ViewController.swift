c4ba1bf419436578227497b60901412ac1e233e2
kinnerapriyap <kinnerapriyap@gmail.com>
2019-11-26 15:45:11 +0900

    Add toolbar items

--- projects-by-day/day-4-easy-browser/day-4-easy-browser/ViewController.swift
+++ projects-by-day/day-4-easy-browser/day-4-easy-browser/ViewController.swift
@@ -22,6 +22,11 @@ class ViewController: UIViewController, WKNavigationDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+        let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
+
+        toolbarItems = [spacer, refresh]
+        navigationController?.isToolbarHidden = false
     }
     
     @objc func openTapped() {
