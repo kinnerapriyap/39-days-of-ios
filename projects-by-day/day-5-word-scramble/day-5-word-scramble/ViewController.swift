101bf9ac241cb9278eb02a5b4973aade3fdd3adb
kinnerapriyap <kinnerapriyap@gmail.com>
2019-11-27 19:09:02 +0900

    Add all the words in file to array

--- projects-by-day/day-5-word-scramble/day-5-word-scramble/ViewController.swift
+++ projects-by-day/day-5-word-scramble/day-5-word-scramble/ViewController.swift
@@ -9,10 +9,22 @@
 import UIKit
 
 class ViewController: UITableViewController {
+    
+    var allWords = [String]()
+    var usedWords = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                allWords = startWords.components(separatedBy: "\n")
+            }
+        }
+
+        if allWords.isEmpty {
+            allWords = ["silkworm"]
+        }
     }
 
 
