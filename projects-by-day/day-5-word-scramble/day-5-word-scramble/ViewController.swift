360f56384ff7a0b344c4241142cf04249d65cc7b
kinnerapriyap <kinnerapriyap@gmail.com>
2019-11-27 20:20:17 +0900

    Check word contains insensitively

--- projects-by-day/day-5-word-scramble/day-5-word-scramble/ViewController.swift
+++ projects-by-day/day-5-word-scramble/day-5-word-scramble/ViewController.swift
@@ -103,7 +103,7 @@ class ViewController: UITableViewController {
     }
     
     func isOriginal(word: String) -> Bool {
-        return !usedWords.contains(word)
+        return !usedWords.contains(where: { $0.caseInsensitiveCompare(word) == .orderedSame })
     }
     
     func isReal(word: String) -> Bool {
