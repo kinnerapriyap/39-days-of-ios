d583f2eddde1b9a815aac76ee29f2baebba3af36
kinnerapriyap <kinnerapriyap@gmail.com>
2019-11-27 20:12:58 +0900

    challenge 2 refactor else to use showErrorMessage

--- projects-by-day/day-5-word-scramble/day-5-word-scramble/ViewController.swift
+++ projects-by-day/day-5-word-scramble/day-5-word-scramble/ViewController.swift
@@ -63,9 +63,6 @@ class ViewController: UITableViewController {
     func submit(_ answer: String) {
         let lowerAnswer = answer.lowercased()
         
-        let errorTitle: String
-        let errorMessage: String
-        
         if isPossible(word: lowerAnswer) {
             if isOriginal(word: lowerAnswer) {
                 if isReal(word: lowerAnswer) {
@@ -74,29 +71,19 @@ class ViewController: UITableViewController {
                         
                         let indexPath = IndexPath(row: 0, section: 0)
                         tableView.insertRows(at: [indexPath], with: .automatic)
-                        
-                        return
                     } else {
-                        errorTitle = "Word not valid"
-                        errorMessage = "Not a word less than 3 letters or the original word!"
+                        showErrorMessage(errorTitle: "Word not valid", errorMessage: "Not a word less than 3 letters or the original word!")
                     }
                 } else {
-                    errorTitle = "Word not recognised"
-                    errorMessage = "You can't just make them up, you know!"
+                    showErrorMessage(errorTitle: "Word not recognised", errorMessage: "You can't just make them up, you know!")
                 }
             } else {
-                errorTitle = "Word used already"
-                errorMessage = "Be more original!"
+                showErrorMessage(errorTitle: "Word used already", errorMessage: "Be more original!")
             }
         } else {
             guard let title = title?.lowercased() else { return }
-            errorTitle = "Word not possible"
-            errorMessage = "You can't spell that word from \(title)"
+            showErrorMessage(errorTitle: "Word not possible", errorMessage: "You can't spell that word from \(title)")
         }
-        
-        let ac = UIAlertController(title: errorTitle, message: errorMessage, preferredStyle: .alert)
-        ac.addAction(UIAlertAction(title: "OK", style: .default))
-        present(ac, animated: true)
     }
     
     func isPossible(word: String) -> Bool {
@@ -127,6 +114,12 @@ class ViewController: UITableViewController {
     
     func isValid(word: String) -> Bool {
         return word != title?.lowercased() && word.utf16.count >= 3
+    }
+    
+    func showErrorMessage(errorTitle: String, errorMessage: String) {
+        let ac = UIAlertController(title: errorTitle, message: errorMessage, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
     }
 }
 
