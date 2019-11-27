e53a5a333f875885e200a9c3a9d178c48f9afd73
kinnerapriyap <kinnerapriyap@gmail.com>
2019-11-27 18:55:25 +0900

    Initial day 5 changes

---
+++ projects-by-day/day-5-word-scramble/day-5-word-scramble/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  day-5-word-scramble
+//
+//  Created by kinnerapriyap on 2019/11/27.
+//  Copyright © 2019 kinnerapriyap. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
