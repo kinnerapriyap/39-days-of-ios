ec742b0b6eeea4c264574c7f5387e21fa7f73686
kinnerapriyap <kinnerapriyap@gmail.com>
2019-11-27 20:14:24 +0900

    challenge 3 add left bar button to refresh word

--- projects-by-day/day-5-word-scramble/day-5-word-scramble/ViewController.swift
+++ projects-by-day/day-5-word-scramble/day-5-word-scramble/ViewController.swift
@@ -18,6 +18,8 @@ class ViewController: UITableViewController {
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
         
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: #selector(startGame))
+        
         if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
             if let startWords = try? String(contentsOf: startWordsURL) {
                 allWords = startWords.components(separatedBy: "\n")
@@ -31,7 +33,7 @@ class ViewController: UITableViewController {
         startGame()
     }
     
-    func startGame() {
+    @objc func startGame() {
         title = allWords.randomElement()
         usedWords.removeAll(keepingCapacity: true)
         tableView.reloadData()
