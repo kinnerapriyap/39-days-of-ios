e349048cb6ba08e7f586a5a28977a1869ed59adf
kinnerapriyap <kinnerapriyap@gmail.com>
2019-11-29 10:01:17 +0900

    day 7 initial commit

---
+++ projects-by-day/day-7-whitehouse-petitions/day-7-whitehouse-petitions/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  day-7-whitehouse-petitions
+//
+//  Created by kinnerapriyap on 2019/11/29.
+//  Copyright © 2019 kinnerapriyap. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
