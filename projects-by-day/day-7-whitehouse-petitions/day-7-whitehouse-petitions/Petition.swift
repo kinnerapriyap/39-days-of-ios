96ad34e9984575e0988ca6f596e27fe5f67ae64c
kinnerapriyap <kinnerapriyap@gmail.com>
2019-11-29 12:11:33 +0900

    Add Petition.swift

---
+++ projects-by-day/day-7-whitehouse-petitions/day-7-whitehouse-petitions/Petition.swift
@@ -0,0 +1,13 @@
+//
+//  Petition.swift
+//  day-7-whitehouse-petitions
+//
+//  Created by kinnerapriyap on 2019/11/29.
+//  Copyright © 2019 kinnerapriyap. All rights reserved.
+//
+
+struct Petition: Codable  {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
