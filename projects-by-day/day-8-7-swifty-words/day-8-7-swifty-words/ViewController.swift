ec0ae4a8a641c6bb050b9206f8147af482b1ca8e
kinnerapriyap <kinnerapriyap@gmail.com>
2019-11-30 11:03:44 +0900

    day 8 initial commit

---
+++ projects-by-day/day-8-7-swifty-words/day-8-7-swifty-words/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  day-8-7-swifty-words
+//
+//  Created by kinnerapriyap on 2019/11/30.
+//  Copyright © 2019 kinnerapriyap. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
