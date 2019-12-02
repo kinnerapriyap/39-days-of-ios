60b80f533e726f9bf2ffe5ef7212555d999dc46d
kinnerapriyap <kinnerapriyap@gmail.com>
2019-12-02 11:04:50 +0900

    Connect PersonCell to storyboard

---
+++ projects-by-day/day-10-names-to-faces/day-10-names-to-faces/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  day-10-names-to-faces
+//
+//  Created by kinnerapriyap on 2019/12/02.
+//  Copyright © 2019 kinnerapriyap. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+}
