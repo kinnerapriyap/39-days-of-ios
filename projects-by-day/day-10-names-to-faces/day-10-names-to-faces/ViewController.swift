d66f32b537136a2f74fd256ba7a0c0a75c0034d4
kinnerapriyap <kinnerapriyap@gmail.com>
2019-12-02 11:35:30 +0900

    Add button to add item

--- projects-by-day/day-10-names-to-faces/day-10-names-to-faces/ViewController.swift
+++ projects-by-day/day-10-names-to-faces/day-10-names-to-faces/ViewController.swift
@@ -8,23 +8,31 @@
 
 import UIKit
 
-class ViewController: UICollectionViewController {
-
+class ViewController: UICollectionViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewPerson))
     }
-
+    
+    @objc func addNewPerson() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+    
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 10
     }
-
+    
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else {
             // we failed to get a PersonCell – bail out!
             fatalError("Unable to dequeue PersonCell.")
         }
-
+        
         // if we're still here it means we got a PersonCell, so we can return it
         return cell
     }
