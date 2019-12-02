d1623392c471f9791a55b795c3fbf8f9d4f10414
kinnerapriyap <kinnerapriyap@gmail.com>
2019-12-02 10:36:03 +0900

    Day 10 initial commit

---
+++ projects-by-day/day-10-names-to-faces/day-10-names-to-faces/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  day-10-names-to-faces
+//
+//  Created by kinnerapriyap on 2019/12/02.
+//  Copyright © 2019 kinnerapriyap. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
