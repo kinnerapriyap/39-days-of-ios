57e1875b185bf443d54f6007c605f315f0fa0e11
kinnerapriyap <kinnerapriyap@gmail.com>
2019-12-06 12:28:55 +0900

    Conform Person class to NSCoding

--- projects-by-day/day-12-user-defaults-a/day-12-user-defaults-a/Person.swift
+++ projects-by-day/day-12-user-defaults-a/day-12-user-defaults-a/Person.swift
@@ -16,4 +16,14 @@ class Person: NSObject {
         self.name = name
         self.image = image
     }
+    
+    required init(coder aDecoder: NSCoder) {
+        name = aDecoder.decodeObject(forKey: "name") as? String ?? ""
+        image = aDecoder.decodeObject(forKey: "image") as? String ?? ""
+    }
+
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(name, forKey: "name")
+        aCoder.encode(image, forKey: "image")
+    }
 }
