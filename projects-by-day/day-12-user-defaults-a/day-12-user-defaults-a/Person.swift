acdac0b90de7cf1b734b10d64f3aa896fe03f51a
kinnerapriyap <kinnerapriyap@gmail.com>
2019-12-06 12:18:19 +0900

    Day 12 initial commits

---
+++ projects-by-day/day-12-user-defaults-a/day-12-user-defaults-a/Person.swift
@@ -0,0 +1,19 @@
+//
+//  Person.swift
+//  day-10-names-to-faces
+//
+//  Created by kinnerapriyap on 2019/12/02.
+//  Copyright © 2019 kinnerapriyap. All rights reserved.
+//
+
+import UIKit
+
+class Person: NSObject {
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+}
