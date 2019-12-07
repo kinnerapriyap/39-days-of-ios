960858ddadbf61cf67b8cba73873e1c4d5846944
kinnerapriyap <kinnerapriyap@gmail.com>
2019-12-07 21:06:50 +0900

    Initialise day 14 and add content files

---
+++ projects-by-day/day-14-whack-a-penguin/day-14-whack-a-penguin/GameScene.swift
@@ -0,0 +1,17 @@
+//
+//  GameScene.swift
+//  day-14-whack-a-penguin
+//
+//  Created by kinnerapriyap on 2019/12/07.
+//  Copyright © 2019 kinnerapriyap. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMove(to view: SKView) {
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+}
