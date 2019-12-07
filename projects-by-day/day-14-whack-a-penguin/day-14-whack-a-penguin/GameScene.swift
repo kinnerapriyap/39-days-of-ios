5507cf4c09ae09155000615940c172e8541a5ff0
kinnerapriyap <kinnerapriyap@gmail.com>
2019-12-07 22:02:20 +0900

    Add logic for hitting penguin

--- projects-by-day/day-14-whack-a-penguin/day-14-whack-a-penguin/GameScene.swift
+++ projects-by-day/day-14-whack-a-penguin/day-14-whack-a-penguin/GameScene.swift
@@ -10,6 +10,8 @@ import SpriteKit
 
 class GameScene: SKScene {
     
+    var numRounds = 0
+    
     var slots = [WhackSlot]()
     
     var gameScore: SKLabelNode!
@@ -45,6 +47,30 @@ class GameScene: SKScene {
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: self)
+        let tappedNodes = nodes(at: location)
+        
+        for node in tappedNodes {
+            guard let whackSlot = node.parent?.parent as? WhackSlot else { continue }
+            if !whackSlot.isVisible { continue }
+            if whackSlot.isHit { continue }
+            whackSlot.hit()
+
+            if node.name == "charFriend" {
+                // they shouldn't have whacked this penguin
+                score -= 5
+
+                run(SKAction.playSoundFileNamed("whackBad.caf", waitForCompletion: false))
+            } else if node.name == "charEnemy" {
+                // they should have whacked this one
+                whackSlot.charNode.xScale = 0.85
+                whackSlot.charNode.yScale = 0.85
+                score += 1
+
+                run(SKAction.playSoundFileNamed("whack.caf", waitForCompletion: false))
+            }
+        }
     }
     
     func createSlot(at position: CGPoint) {
@@ -55,20 +81,35 @@ class GameScene: SKScene {
     }
     
     func createEnemy() {
-        popupTime *= 0.991
+        numRounds += 1
 
+        if numRounds >= 30 {
+            for slot in slots {
+                slot.hide()
+            }
+
+            let gameOver = SKSpriteNode(imageNamed: "gameOver")
+            gameOver.position = CGPoint(x: 512, y: 384)
+            gameOver.zPosition = 1
+            addChild(gameOver)
+
+            return
+        }
+        
+        popupTime *= 0.991
+        
         slots.shuffle()
         slots[0].show(hideTime: popupTime)
-
+        
         if Int.random(in: 0...12) > 4 { slots[1].show(hideTime: popupTime) }
         if Int.random(in: 0...12) > 8 {  slots[2].show(hideTime: popupTime) }
         if Int.random(in: 0...12) > 10 { slots[3].show(hideTime: popupTime) }
         if Int.random(in: 0...12) > 11 { slots[4].show(hideTime: popupTime)  }
-
+        
         let minDelay = popupTime / 2.0
         let maxDelay = popupTime * 2
         let delay = Double.random(in: minDelay...maxDelay)
-
+        
         DispatchQueue.main.asyncAfter(deadline: .now() + delay) { [weak self] in
             self?.createEnemy()
         }
