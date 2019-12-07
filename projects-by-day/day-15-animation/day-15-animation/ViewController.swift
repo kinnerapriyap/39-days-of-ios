c314ab4d22956db7d1e5f59772638ac859be1a20
kinnerapriyap <kinnerapriyap@gmail.com>
2019-12-07 22:45:39 +0900

    Day 15 initial ocmmit

---
+++ projects-by-day/day-15-animation/day-15-animation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  day-15-animation
+//
+//  Created by kinnerapriyap on 2019/12/07.
+//  Copyright © 2019 kinnerapriyap. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
