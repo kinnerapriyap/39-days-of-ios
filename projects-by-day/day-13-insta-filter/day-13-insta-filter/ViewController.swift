d950c11ab702527d9ea3773b03ac68e50e662e4b
kinnerapriyap <kinnerapriyap@gmail.com>
2019-12-07 20:15:49 +0900

    Design layout and set outlets

---
+++ projects-by-day/day-13-insta-filter/day-13-insta-filter/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  day-13-insta-filter
+//
+//  Created by kinnerapriyap on 2019/12/07.
+//  Copyright © 2019 kinnerapriyap. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var intensity: UISlider!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func changeFilter(_ sender: Any) {
+    }
+    
+    @IBAction func save(_ sender: Any) {
+    }
+}
+
