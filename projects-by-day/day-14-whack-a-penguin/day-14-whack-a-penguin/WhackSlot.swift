ffdbd8a1dea0fde5abb762666d10d30e7b4c7768
kinnerapriyap <kinnerapriyap@gmail.com>
2019-12-07 21:17:00 +0900

    Hide penguins in holes

---
+++ projects-by-day/day-14-whack-a-penguin/day-14-whack-a-penguin/WhackSlot.swift
@@ -0,0 +1,34 @@
+//
+//  WhackSlot.swift
+//  day-14-whack-a-penguin
+//
+//  Created by kinnerapriyap on 2019/12/07.
+//  Copyright © 2019 kinnerapriyap. All rights reserved.
+//
+
+import SpriteKit
+import UIKit
+
+class WhackSlot: SKNode {
+    
+    var charNode: SKSpriteNode!
+    
+    func configure(at position: CGPoint) {
+        self.position = position
+        
+        let sprite = SKSpriteNode(imageNamed: "whackHole")
+        addChild(sprite)
+        
+        let cropNode = SKCropNode()
+        cropNode.position = CGPoint(x: 0, y: 15)
+        cropNode.zPosition = 1
+        cropNode.maskNode = SKSpriteNode(imageNamed: "whackMask")
+        
+        charNode = SKSpriteNode(imageNamed: "penguinGood")
+        charNode.position = CGPoint(x: 0, y: -90)
+        charNode.name = "character"
+        cropNode.addChild(charNode)
+        
+        addChild(cropNode)
+    }
+}
