e678f2c0b92ea743d436ea2b431dbbbf4a0606a4
kinnerapriyap <kinnerapriyap@gmail.com>
2019-12-08 16:37:57 +0900

    Add custom views to click on places

--- projects-by-day/day-16-capital-cities/day-16-capital-cities/ViewController.swift
+++ projects-by-day/day-16-capital-cities/day-16-capital-cities/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
     
     @IBOutlet weak var mapView: MKMapView!
     
@@ -25,6 +25,40 @@ class ViewController: UIViewController {
         mapView.addAnnotations([london, oslo, paris, rome, washington])
     }
     
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        // 1
+        guard annotation is Capital else { return nil }
+        
+        // 2
+        let identifier = "Capital"
+        
+        // 3
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
+        
+        if annotationView == nil {
+            //4
+            annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+            annotationView?.canShowCallout = true
+            
+            // 5
+            let btn = UIButton(type: .detailDisclosure)
+            annotationView?.rightCalloutAccessoryView = btn
+        } else {
+            // 6
+            annotationView?.annotation = annotation
+        }
+        
+        return annotationView
+    }
     
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        guard let capital = view.annotation as? Capital else { return }
+        let placeName = capital.title
+        let placeInfo = capital.info
+        
+        let ac = UIAlertController(title: placeName, message: placeInfo, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
 }
 
