b324278a9974a9ef32fe948275a2a12da0b5c617
kinnerapriyap <kinnerapriyap@gmail.com>
2019-12-08 16:13:37 +0900

    Day 16 initial commit with map view

---
+++ projects-by-day/day-16-capital-cities/day-16-capital-cities/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  day-16-capital-cities
+//
+//  Created by kinnerapriyap on 2019/12/08.
+//  Copyright © 2019 kinnerapriyap. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
