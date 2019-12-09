b5e5d7c6e7914c1ee8d59422874902281bce6127
kinnerapriyap <kinnerapriyap@gmail.com>
2019-12-09 23:03:57 +0900

    Day 17 initial commit to add contents and clean code

---
+++ projects-by-day/day-17-space-race/day-17-space-race/GameScene.swift
@@ -0,0 +1,16 @@
+//
+//  GameScene.swift
+//  day-17-space-race
+//
+//  Created by kinnerapriyap on 2019/12/09.
+//  Copyright © 2019 kinnerapriyap. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+}
