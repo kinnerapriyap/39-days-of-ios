b06221b15ca0584b99173541ed8491c322a73cf3
kinnerapriyap <kinnerapriyap@gmail.com>
2019-12-10 13:53:48 +0900

    Day 18 initial commit

---
+++ projects-by-day/day-18-debugging/day-18-debugging/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  day-18-debugging
+//
+//  Created by kinnerapriyap on 2019/12/10.
+//  Copyright © 2019 kinnerapriyap. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
