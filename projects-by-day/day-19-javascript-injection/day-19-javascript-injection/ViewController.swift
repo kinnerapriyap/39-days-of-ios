3ede3cb5cfb13903b25e9edb806a650c00a6946d
kinnerapriyap <kinnerapriyap@gmail.com>
2019-12-12 17:26:18 +0900

    day 19 initial commit

---
+++ projects-by-day/day-19-javascript-injection/day-19-javascript-injection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  day-19-javascript-injection
+//
+//  Created by kinnerapriyap on 2019/12/12.
+//  Copyright © 2019 kinnerapriyap. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
