05870689d72d7f28104beee8899725d264e4eecd
kinnerapriyap <kinnerapriyap@gmail.com>
2019-12-15 09:59:05 +0900

    Initialise and clean up day 20

---
+++ projects-by-day/day-20-fireworks-night/day-20-fireworks-night/GameScene.swift
@@ -0,0 +1,17 @@
+//
+//  GameScene.swift
+//  day-20-fireworks-night
+//
+//  Created by kinnerapriyap on 2019/12/15.
+//  Copyright © 2019 kinnerapriyap. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+}
