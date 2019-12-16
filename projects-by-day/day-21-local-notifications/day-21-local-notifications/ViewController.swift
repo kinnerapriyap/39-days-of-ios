08d92fa08b7a6a9dd8ab1266254d4c25c29f7683
kinnerapriyap <kinnerapriyap@gmail.com>
2019-12-16 17:16:44 +0900

    Initialise day 21

---
+++ projects-by-day/day-21-local-notifications/day-21-local-notifications/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  day-21-local-notifications
+//
+//  Created by kinnerapriyap on 2019/12/16.
+//  Copyright © 2019 kinnerapriyap. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
