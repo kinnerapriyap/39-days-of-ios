79de36a392604cb7dde6c4a7ccdf663db84d50a3
kinnerapriyap <kinnerapriyap@gmail.com>
2019-12-17 22:22:53 +0900

    Add more extensions for strings

--- projects-by-day/day-24-swift-strings.playground/Contents.swift
+++ projects-by-day/day-24-swift-strings.playground/Contents.swift
@@ -5,6 +5,22 @@ extension String {
     subscript(i: Int) -> String {
         return String(self[index(startIndex, offsetBy: i)])
     }
+    // remove a prefix if it exists
+    func deletingPrefix(_ prefix: String) -> String {
+        guard self.hasPrefix(prefix) else { return self }
+        return String(self.dropFirst(prefix.count))
+    }
+
+    // remove a suffix if it exists
+    func deletingSuffix(_ suffix: String) -> String {
+        guard self.hasSuffix(suffix) else { return self }
+        return String(self.dropLast(suffix.count))
+    }
+    
+    var capitalizedFirst: String {
+        guard let firstLetter = self.first else { return "" }
+        return firstLetter.uppercased() + self.dropFirst()
+    }
 }
 var name = "Hello, playground"
 let letter = name[3]
