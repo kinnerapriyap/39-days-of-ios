2a857100a1939d0df5f036635fc1150fbc15a037
kinnerapriyap <kinnerapriyap@gmail.com>
2020-01-01 09:06:36 +0900

    Initial day 28 commit

---
+++ projects-by-day/day-28-secret-swift/day-28-secret-swift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  day-28-secret-swift
+//
+//  Created by kinnerapriyap on 2020/01/01.
+//  Copyright © 2020 kinnerapriyap. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
