c849b9d9b02e2c9b44908c35b052b99a332671ea
kinnerapriyap <kinnerapriyap@gmail.com>
2020-01-06 18:46:55 +0900

    Add remove functionality

--- projects-by-day/day-31-multibrowser/day-31-multibrowser/ViewController.swift
+++ projects-by-day/day-31-multibrowser/day-31-multibrowser/ViewController.swift
@@ -69,6 +69,31 @@ class ViewController: UIViewController, WKNavigationDelegate, UITextFieldDelegat
     }
     
     @objc func deleteWebView() {
+        // safely unwrap our webview
+        if let webView = activeWebView {
+            if let index = stackView.arrangedSubviews.firstIndex(of: webView) {
+                // We found the webview – remove it from the stack view and destroy it
+                webView.removeFromSuperview()
+
+                if stackView.arrangedSubviews.count == 0 {
+                    // go back to our default UI
+                    setDefaultTitle()
+                } else {
+                    // convert the Index value into an integer
+                    var currentIndex = Int(index)
+
+                    // if that was the last web view in the stack, go back one
+                    if currentIndex == stackView.arrangedSubviews.count {
+                        currentIndex = stackView.arrangedSubviews.count - 1
+                    }
+
+                    // find the web view at the new index and select it
+                    if let newSelectedWebView = stackView.arrangedSubviews[currentIndex] as? WKWebView {
+                        selectWebView(newSelectedWebView)
+                    }
+                }
+            }
+        }
     }
     
     @objc func webViewTapped(_ recognizer: UITapGestureRecognizer) {
