831afaddb942ef4ab78f1773c0773f2727eb6446
kinnerapriyap <kinnerapriyap@gmail.com>
2020-01-06 13:25:45 +0900

    Add ellipses and checkerboards

--- projects-by-day/day-27-core-graphics/day-27-core-graphics/ViewController.swift
+++ projects-by-day/day-27-core-graphics/day-27-core-graphics/ViewController.swift
@@ -35,6 +35,41 @@ class ViewController: UIViewController {
         }
         imageView.image = img
     }
+    
+    func drawCircle() {
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
+
+        let img = renderer.image { ctx in
+            let rectangle = CGRect(x: 5, y: 5, width: 502, height: 502)
+
+            ctx.cgContext.setFillColor(UIColor.red.cgColor)
+            ctx.cgContext.setStrokeColor(UIColor.black.cgColor)
+            ctx.cgContext.setLineWidth(10)
+
+            ctx.cgContext.addEllipse(in: rectangle)
+            ctx.cgContext.drawPath(using: .fillStroke)
+        }
+
+        imageView.image = img
+    }
+    
+    func drawCheckerboard() {
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
+
+        let img = renderer.image { ctx in
+            ctx.cgContext.setFillColor(UIColor.black.cgColor)
+
+            for row in 0 ..< 8 {
+                for col in 0 ..< 8 {
+                    if (row + col) % 2 == 0 {
+                        ctx.cgContext.fill(CGRect(x: col * 64, y: row * 64, width: 64, height: 64))
+                    }
+                }
+            }
+        }
+
+        imageView.image = img
+    }
 
     @IBAction func redrawTapped(_ sender: Any) {
         currentDrawType += 1
@@ -46,6 +81,12 @@ class ViewController: UIViewController {
         switch currentDrawType {
         case 0:
             drawRectangle()
+
+        case 1:
+            drawCircle()
+            
+        case 2:
+            drawCheckerboard()
 
         default:
             break
