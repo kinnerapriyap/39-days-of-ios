126af0a5f420241b10070ca67b9d2b66405cec45
kinnerapriyap <kinnerapriyap@gmail.com>
2020-01-06 12:12:41 +0900

    Clean up initial commit day 23

---
+++ projects-by-day/day-23-swifty-ninja/day-23-swifty-ninja/GameScene.swift
@@ -0,0 +1,17 @@
+//
+//  GameScene.swift
+//  day-23-swifty-ninja
+//
+//  Created by kinnerapriyap on 2020/01/06.
+//  Copyright © 2020 kinnerapriyap. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMove(to view: SKView) {
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+}
