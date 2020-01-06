2fdfce28d31b919eb9729caa58ecbd3950ec290b
kinnerapriyap <kinnerapriyap@gmail.com>
2020-01-06 12:50:47 +0900

    initial and clean up day 26

---
+++ projects-by-day/day-26-marble-maze/day-26-marble-maze/GameScene.swift
@@ -0,0 +1,13 @@
+//
+//  GameScene.swift
+//  day-26-marble-maze
+//
+//  Created by kinnerapriyap on 2020/01/06.
+//  Copyright © 2020 kinnerapriyap. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+
+}
