70bda1de750bf204f49c6ad36651dcfbc23c1d92
kinnerapriyap <kinnerapriyap@gmail.com>
2020-01-06 12:43:30 +0900

    Add SKTexture

--- projects-by-day/day-23-swifty-ninja/day-23-swifty-ninja/GameScene.swift
+++ projects-by-day/day-23-swifty-ninja/day-23-swifty-ninja/GameScene.swift
@@ -44,6 +44,8 @@ class GameScene: SKScene {
     var chainDelay = 3.0
     var nextSequenceQueued = true
     
+    var isGameEnded = false
+    
     override func didMove(to view: SKView) {
         let background = SKSpriteNode(imageNamed: "sliceBackground")
         background.position = CGPoint(x: 512, y: 384)
@@ -72,6 +74,10 @@ class GameScene: SKScene {
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if isGameEnded {
+            return
+        }
+        
         guard let touch = touches.first else { return }
         let location = touch.location(in: self)
         activeSlicePoints.append(location)
@@ -297,51 +303,53 @@ class GameScene: SKScene {
         activeEnemies.append(enemy)
     }
     
-    override func update(_ currentTime: TimeInterval) {
-        if activeEnemies.count > 0 {
-            for (index, node) in activeEnemies.enumerated().reversed() {
-                if node.position.y < -140 {
-                node.removeAllActions()
+    func subtractLife() {
+        lives -= 1
 
-                if node.name == "enemy" {
-                    node.name = ""
-                    subtractLife()
+        run(SKAction.playSoundFileNamed("wrong.caf", waitForCompletion: false))
 
-                    node.removeFromParent()
-                    activeEnemies.remove(at: index)
-                } else if node.name == "bombContainer" {
-                    node.name = ""
-                    node.removeFromParent()
-                    activeEnemies.remove(at: index)
-                }
-            }
+        var life: SKSpriteNode
+
+        if lives == 2 {
+            life = livesImages[0]
+        } else if lives == 1 {
+            life = livesImages[1]
         } else {
-            if !nextSequenceQueued {
-                DispatchQueue.main.asyncAfter(deadline: .now() + popupTime) { [weak self] in
-                    self?.tossEnemies()
-                }
-
-                nextSequenceQueued = true
-            }
+            life = livesImages[2]
+            endGame(triggeredByBomb: false)
         }
+
+        life.texture = SKTexture(imageNamed: "sliceLifeGone")
+
+        life.xScale = 1.3
+        life.yScale = 1.3
+        life.run(SKAction.scale(to: 1, duration:0.1))
+    }
         
-        var bombCount = 0
-
-        for node in activeEnemies {
-            if node.name == "bombContainer" {
-                bombCount += 1
-                break
-            }
+    func endGame(triggeredByBomb: Bool) {
+        if isGameEnded {
+            return
         }
 
-        if bombCount == 0 {
-            // no bombs – stop the fuse sound!
-            bombSoundEffect?.stop()
-            bombSoundEffect = nil
+        isGameEnded = true
+        physicsWorld.speed = 0
+        isUserInteractionEnabled = false
+
+        bombSoundEffect?.stop()
+        bombSoundEffect = nil
+
+        if triggeredByBomb {
+            livesImages[0].texture = SKTexture(imageNamed: "sliceLifeGone")
+            livesImages[1].texture = SKTexture(imageNamed: "sliceLifeGone")
+            livesImages[2].texture = SKTexture(imageNamed: "sliceLifeGone")
         }
     }
     
     func tossEnemies() {
+        if isGameEnded {
+            return
+        }
+        
         popupTime *= 0.991
         chainDelay *= 0.99
         physicsWorld.speed *= 1.02
@@ -393,5 +401,50 @@ class GameScene: SKScene {
 
         sequencePosition += 1
         nextSequenceQueued = false
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        if activeEnemies.count > 0 {
+            for (index, node) in activeEnemies.enumerated().reversed() {
+                if node.position.y < -140 {
+                node.removeAllActions()
+
+                if node.name == "enemy" {
+                    node.name = ""
+                    subtractLife()
+
+                    node.removeFromParent()
+                    activeEnemies.remove(at: index)
+                } else if node.name == "bombContainer" {
+                    node.name = ""
+                    node.removeFromParent()
+                    activeEnemies.remove(at: index)
+                }
+                }
+            }
+        }else {
+            if !self.nextSequenceQueued {
+                DispatchQueue.main.asyncAfter(deadline: .now() + self.popupTime) { [weak self] in
+                    self?.tossEnemies()
+                }
+
+                self.nextSequenceQueued = true
+            }
+        }
+        
+        var bombCount = 0
+
+        for node in activeEnemies {
+            if node.name == "bombContainer" {
+                bombCount += 1
+                break
+            }
+        }
+
+        if bombCount == 0 {
+            // no bombs – stop the fuse sound!
+            bombSoundEffect?.stop()
+            bombSoundEffect = nil
+        }
     }
 }
