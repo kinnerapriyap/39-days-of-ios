554e3ee43f229a89fd578e745152471c660c9ea2
kinnerapriyap <kinnerapriyap@gmail.com>
2020-01-07 11:49:03 +0900

    day 32 initial commit

---
+++ projects-by-day/day-32-swift-searcher/day-32-swift-searcher/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  day-32-swift-searcher
+//
+//  Created by kinnerapriyap on 2020/01/07.
+//  Copyright © 2020 kinnerapriyap. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
